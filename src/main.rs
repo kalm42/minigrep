81c1c27ed0d359f865ae2b027928d7c15b615207
Kyle Melton <kalm42@gmail.com>
2020-07-31 17:44:39 -0600

    12-5

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
+    let query = &args[1]; // pass reference to 2nd item in vector leaving ownership with args
+    let filename = &args[2]; // samething, but the 3rd tiem
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
+
+fn parse_config(args: &[String]) => (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+  
+    (query, filename)
+  }
